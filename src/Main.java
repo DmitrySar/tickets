425e2df7d3e1110b0a76e8474c55f1abc93822ec
DmitrySar <youshin.d@gmail.com>
2021-12-05 17:04:59 +0300

    edit Main

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,11 @@ public class Main {
 		students.add(new Student("иванов иван иванович", 303));
 		students.add(new Student("петров петр петрович", 303));
 		students.add(new Student("сидоров сидор сидорович", 306));
+		students.add(new Student("feefdfdf gfrrrgr rgrgrg", 303));
 		tickets.add(new Ticket(1, "link 1"));
 		tickets.add(new Ticket(2, "link 2"));
 		tickets.add(new Ticket(3, "link 3"));
+		tickets.add(new Ticket(4, "link 4"));
 		Distributor distributor = new Distributor(students, tickets);
 		for (Student s: distributor.getStudentsWithTickets()) {
 			String line = String.format("%s\tбилет N%s\t%s",
